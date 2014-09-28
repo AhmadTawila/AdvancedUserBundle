e8d9e0aaaba3b8803ba5e81e0d7283be1b2efefd
Ahmad Tawila <ahmad@tawila.net>
2014-09-28 17:17:52 +0200

    Initial commit

---
+++ AdvancedUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tawila\AdvancedUserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdvancedUserBundle extends Bundle
+{
+}
---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tawila\AdvancedUserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('AdvancedUserBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
