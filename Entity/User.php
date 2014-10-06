d4aff57fc762237ce6c2ca4edb6d9c98e4b6b866
Ahmad Tawila <ahmad@tawila.net>
2014-10-06 23:25:22 +0200

    Add the sub-class for User entity

---
+++ Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tawila\AdvancedUserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="egrep_user")
+ * @ORM\Entity(repositoryClass="Tawila\AdvancedUserBundle\Entity\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
